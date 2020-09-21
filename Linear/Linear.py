46d95fe17263c3e953f649e35b46439939f039bc
BeckenbauerRijiirkd <beckraika@gmail.com>
2020-09-20 22:14:39 -0300

    Update Linear.py

--- Linear/Linear.py
+++ Linear/Linear.py
@@ -1,9 +1,9 @@
 def BuscaLinear ():
     k = 1
     vetor = [10,2,3,4,5]
-    selecionado = int(input("numero buscado: "))
+    select = int(input("numero buscado: "))
     for i in range (0, len(vetor)):
-        if selecionado == vetor[i]:
+        if select == vetor[i]:
             print("O elemento esta na posição: ",(i+1))
             k =k+1
     if k ==1:
