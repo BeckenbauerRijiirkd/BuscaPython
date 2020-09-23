bb5144b1b281fb51d14afb8f3add0ff552cb804a
BeckenbauerRijiirkd <beckraika@gmail.com>
2020-09-22 22:52:32 -0300

    Update Linear.py

--- Linear/Linear.py
+++ Linear/Linear.py
@@ -1,12 +1,22 @@
 def BuscaLinear ():
+    import time
     k = 1
-    vetor = [10,2,3,4,5]
-    select = int(input("numero buscado: "))
+    vetor =[]
+    tamanho = int(input("quantidade de elementos: "))
+    for i in range (0, tamanho):
+        print("Elemento", i+1)
+        digito = int(input())
+        vetor.append(digito)
+    print(vetor)
+    select = int(input("Numero a Ser Buscado: "))
+    t1 = time.time()
     for i in range (0, len(vetor)):
         if select == vetor[i]:
             print("O elemento esta na posição: ",(i+1))
             k =k+1
     if k ==1:
         print("Elemento não encontrado")
+    tempoExec = time.time() - t1
+    print("Tempo de execução: {:.5f} segundos".format(tempoExec))
 
 BuscaLinear()
